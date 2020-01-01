17dfab8d882a461706e76460eeaa6848ee1da089
Aaron Burdick <aburd@applenoMacBook-Pro.local>
2020-01-02 01:15:53 +0900

    Implement most rules

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,76 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern {}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    let greeting = format!("Hello, {}", name);
-    alert(greeting.as_str());
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
 }
 
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter() {
+            for delta_column in [self.width - 1, 0, 1].iter() {
+                if (delta_row == 0 && delta_column == 0) {
+                    continue;
+                }
+                let neighbor_row = row + delta_row % self.height;
+                let neighbor_column = column + delta_column % self.width;
+                let neighbor_index = self.get_index(neighbor_row, neighbor_column);
+                count += self.cells[neighbor_index] as u8;
+            }
+        }
+        count
+    }
+}
+
+/// Public methods, exported to JavaScript.
 #[wasm_bindgen]
-pub fn another_greeting() {
-    alert("Trying out new stuff");
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for column in 0..self.width {
+                let idx = self.get_index(row, column);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, column);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, neighbors) if neighbors < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, neighbors) if (neighbors == 2 || neighbors == 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, neighbors) if neighbors > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, neighbors) if neighbors == 3 => Cell::Alive,
+                    // otherwise, same state
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next;
+    }
 }
